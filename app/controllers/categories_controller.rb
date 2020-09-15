798f58a1772e1293051322fd7c41ed935af018c1
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-15 15:33:03 -0500

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,23 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def create
+    @category = Category.create(category_params)
+      if @category.save
+        redirect_to category_path(@category)
+      else
+        render :new
+      end
+  end
 
   def show
-    @category = Category.find(params[:id])
+    @category = Category.find_by_id(params[:id])
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
